4277758e51eb648607884c4ab3fad9ada23fe37b
Soumadip Ghosh <sdsghosh1995@gmail.com>
2026-01-16 23:30:33 +0530

    Add root health check endpoint

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("App is running successfully")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
